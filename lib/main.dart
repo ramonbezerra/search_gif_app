d7095e2eebc97a522eb27aa3aab45d4813fb4740
ramonbezerra <57471737+ramonbezerra@users.noreply.github.com>
2020-04-15 15:40:03 -0300

    build gifs search and image grid

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:search_gif_app/ui/home_page.dart';
 
-void main() => runApp(MaterialApp(home: HomePage()));
+void main() => runApp(MaterialApp(
+      home: HomePage(),
+      theme: ThemeData(hintColor: Colors.white),
+    ));
--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -9,32 +9,95 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final _textSearchController = TextEditingController();
   String _search;
   int _offset = 0;
 
-  Future<Map> _getGifs() async{
+  Future<Map> _getGifs() async {
     http.Response response;
+    setState(() {
+      _search = _textSearchController.text;
+    });
 
-    if (_search == null) 
-      response = await http.get("https://api.giphy.com/v1/gifs/trending?api_key=2asxi8jCjm42SdLyaLcttn24IVwAuDzE&limit=25&rating=G");
-    else 
-      response = await http.get("https://api.giphy.com/v1/gifs/search?api_key=2asxi8jCjm42SdLyaLcttn24IVwAuDzE&q=$_search&limit=10&$_offset=10&rating=G&lang=en");
-    
+    if (_search == null)
+      response = await http.get(
+          "https://api.giphy.com/v1/gifs/trending?api_key=2asxi8jCjm42SdLyaLcttn24IVwAuDzE&limit=25&rating=G");
+    else
+      response = await http.get(
+          "https://api.giphy.com/v1/gifs/search?api_key=2asxi8jCjm42SdLyaLcttn24IVwAuDzE&q=$_search&limit=10&offset=$_offset&rating=G&lang=pt");
+
     return json.decode(response.body);
   }
 
   @override
-  void initState() {
-    super.initState();
-    _getGifs().then((map) {
-      print(map);
-    });
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.black,
+          title: Image.network(
+              "https://developers.giphy.com/static/img/dev-logo-lg.7404c00322a8.gif"),
+          centerTitle: true,
+        ),
+        backgroundColor: Colors.black,
+        body: Column(
+          children: <Widget>[
+            Padding(
+              padding: EdgeInsets.all(10.0),
+              child: TextField(
+                onSubmitted: (text) {_getGifs();},
+                controller: _textSearchController,
+                decoration: InputDecoration(                    
+                    labelText: "Pesquise aqui... ",
+                    alignLabelWithHint: true,
+                    labelStyle: TextStyle(color: Colors.white),
+                    border: OutlineInputBorder(borderSide: BorderSide(width: 1.0))
+                ),
+                style: TextStyle(color: Colors.white, fontSize: 18.0),
+                textAlign: TextAlign.center,
+              ),
+            ),
+            Expanded(
+              child: FutureBuilder(
+                future: _getGifs(),
+                builder: (context, snapshot) {
+                  switch (snapshot.connectionState) {
+                    case ConnectionState.waiting:
+                    case ConnectionState.none:
+                      return Container(
+                        width: 200.0,
+                        height: 200.0,
+                        alignment: Alignment.center,
+                        child: CircularProgressIndicator(
+                          valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
+                          strokeWidth: 5.0,
+                        ),
+                      );
+                    default:
+                      if(snapshot.hasError) return Container();
+                      else return _createGifTable(context, snapshot);
+                  }
+                }, 
+              )
+            )
+          ],
+        ));
   }
 
-  @override
-  Widget build(BuildContext context) {
-    return Container(    
-      
+  Widget _createGifTable(BuildContext context, AsyncSnapshot snapshot) {
+    return GridView.builder(
+      padding: EdgeInsets.all(10.0),
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        crossAxisSpacing: 10.0,
+        mainAxisSpacing: 10.0
+      ),
+      itemCount: snapshot.data["data"].length,
+      itemBuilder: (context, index) {
+        return GestureDetector(
+          child: Image.network(snapshot.data["data"][index]["images"]["fixed_height_small"]["url"],
+          height: 300.0, fit: BoxFit.cover,),
+        );
+      }
     );
   }
 }
