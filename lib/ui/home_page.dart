9978e26a7d8426df22dd2046bfdc376858210f84
ramonbezerra <57471737+ramonbezerra@users.noreply.github.com>
2020-04-15 16:07:36 -0300

    gif details page and share

--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -2,6 +2,9 @@ import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:share/share.dart';
+
+import 'gif_page.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -21,7 +24,7 @@ class _HomePageState extends State<HomePage> {
           "https://api.giphy.com/v1/gifs/trending?api_key=2asxi8jCjm42SdLyaLcttn24IVwAuDzE&limit=25&rating=G");
     else
       response = await http.get(
-          "https://api.giphy.com/v1/gifs/search?api_key=2asxi8jCjm42SdLyaLcttn24IVwAuDzE&q=$_search&limit=10&offset=$_offset&rating=G&lang=pt");
+          "https://api.giphy.com/v1/gifs/search?api_key=2asxi8jCjm42SdLyaLcttn24IVwAuDzE&q=$_search&limit=9&offset=$_offset&rating=G&lang=pt");
 
     return json.decode(response.body);
   }
@@ -44,61 +47,107 @@ class _HomePageState extends State<HomePage> {
                 onSubmitted: (text) {
                   setState(() {
                     _search = text;
+                    _offset = 0;
                   });
                 },
                 controller: _textSearchController,
-                decoration: InputDecoration(                    
+                decoration: InputDecoration(
                     labelText: "Pesquise aqui... ",
                     alignLabelWithHint: true,
                     labelStyle: TextStyle(color: Colors.white),
-                    border: OutlineInputBorder(borderSide: BorderSide(width: 1.0))
-                ),
+                    border:
+                        OutlineInputBorder(borderSide: BorderSide(width: 1.0))),
                 style: TextStyle(color: Colors.white, fontSize: 18.0),
                 textAlign: TextAlign.center,
               ),
             ),
             Expanded(
-              child: FutureBuilder(
-                future: _getGifs(),
-                builder: (context, snapshot) {
-                  switch (snapshot.connectionState) {
-                    case ConnectionState.waiting:
-                    case ConnectionState.none:
-                      return Container(
-                        width: 200.0,
-                        height: 200.0,
-                        alignment: Alignment.center,
-                        child: CircularProgressIndicator(
-                          valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
-                          strokeWidth: 5.0,
-                        ),
-                      );
-                    default:
-                      if(snapshot.hasError) return Container();
-                      else return _createGifTable(context, snapshot);
-                  }
-                }, 
-              )
-            )
+                child: FutureBuilder(
+              future: _getGifs(),
+              builder: (context, snapshot) {
+                switch (snapshot.connectionState) {
+                  case ConnectionState.waiting:
+                  case ConnectionState.none:
+                    return Container(
+                      width: 200.0,
+                      height: 200.0,
+                      alignment: Alignment.center,
+                      child: CircularProgressIndicator(
+                        valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
+                        strokeWidth: 5.0,
+                      ),
+                    );
+                  default:
+                    if (snapshot.hasError)
+                      return Container();
+                    else
+                      return _createGifTable(context, snapshot);
+                }
+              },
+            )),
           ],
         ));
   }
 
+  int _getCount(List data) {
+    if (_search == null)
+      return data.length;
+    else
+      return data.length + 1;
+  }
+
   Widget _createGifTable(BuildContext context, AsyncSnapshot snapshot) {
     return GridView.builder(
-      padding: EdgeInsets.all(10.0),
-      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-        crossAxisCount: 2,
-        crossAxisSpacing: 10.0,
-        mainAxisSpacing: 10.0
-      ),
-      itemCount: snapshot.data["data"].length,
-      itemBuilder: (context, index) {
-        return GestureDetector(
-          child: Image.network(snapshot.data["data"][index]["images"]["fixed_height_small"]["url"],
-          height: 300.0, fit: BoxFit.cover,),
-        );
-      }
-    );
+        padding: EdgeInsets.all(10.0),
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2, crossAxisSpacing: 10.0, mainAxisSpacing: 10.0),
+        itemCount: _getCount(snapshot.data["data"]),
+        itemBuilder: (context, index) {
+          if (_search == null || index < snapshot.data["data"].length) {
+            return GestureDetector(
+              onTap: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) =>
+                            GifPage(snapshot.data["data"][index])));
+              },
+              onLongPress: () {
+                Share.share(snapshot.data["data"][index]["images"]
+                    ["fixed_height"]["url"]);
+              },
+              child: Image.network(
+                snapshot.data["data"][index]["images"]["fixed_height_small"]
+                    ["url"],
+                height: 300.0,
+                fit: BoxFit.cover,
+              ),
+            );
+          } else {
+            return Container(
+              child: GestureDetector(
+                onTap: () {
+                  setState(() {
+                    _offset += 9;
+                  });
+                },
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Icon(
+                      Icons.add,
+                      color: Colors.white,
+                      size: 70.0,
+                    ),
+                    Text(
+                      "Carregar mais... ",
+                      style: TextStyle(color: Colors.white, fontSize: 22.0),
+                    )
+                  ],
+                ),
+              ),
+            );
+          }
+        });
   }
 }
